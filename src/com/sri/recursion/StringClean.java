525e80ffb116b9be449d6f790006b55f9929df8e
sriramede9 <48334851+sriramede9@users.noreply.github.com>
2021-04-28 12:01:34 -0400

    eliminate duplicate characters by recursion

---
+++ src/com/sri/recursion/StringClean.java
@@ -0,0 +1,21 @@
+package com.sri.recursion;
+
+public class StringClean {
+
+	public String stringClean(String str) {
+
+		if (str.equals("")) {
+			return str;
+		}
+		if (str.length() == 1) {
+			return str;
+		}
+
+		if ((str.charAt(0) == str.charAt(1))) {
+//			System.out.println(str.substring(1));
+			return stringClean(str.substring(1));
+		}
+
+		return str.charAt(0) + stringClean(str.substring(1));
+	}
+}
