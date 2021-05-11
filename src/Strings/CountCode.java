57ef72dbc33d8c774c706a332279e0de3f505981
sriramede9 <48334851+sriramede9@users.noreply.github.com>
2021-05-11 15:00:41 -0400

    map and filter in streams

--- src/Strings/CountCode.java
+++ src/Strings/CountCode.java
@@ -8,17 +8,22 @@ public class CountCode {
 	public int countCode(String str) {
 		int count = 0;
 		for (int i = 0; i < str.length() - 3; i++) {
-			if ((str.substring(i, i + 2) + str.charAt(i+3)).equals("coe")) {
+			if ((str.substring(i, i + 2) + str.charAt(i + 3)).equals("coe")) {
 
 				count++;
 			}
-			System.out.println((str.substring(i, i + 2) + str.charAt(i+3)));
+			System.out.println((str.substring(i, i + 2) + str.charAt(i + 3)));
 		}
 
 		return count;
 	}
 
 	public List<String> no34(List<String> strings) {
-		  return strings.stream().filter(x -> !(x.length() == 3 || x.length() ==4)).collect(Collectors.toList());
-		}
+		return strings.stream().filter(x -> !(x.length() == 3 || x.length() == 4)).collect(Collectors.toList());
+	}
+
+	public List<String> noYY(List<String> strings) {
+
+		return strings.stream().map(x -> x + "y").filter(x -> !(x.contains("yy"))).collect(Collectors.toList());
+	}
 }
